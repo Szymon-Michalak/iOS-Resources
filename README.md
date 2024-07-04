8c46fe4f0d289b9946eae7b6708e0755cd14d64f
Szymon Michalak <77302574+Szymon-Michalak@users.noreply.github.com>
2024-07-04 23:53:41 +0200

    Style Astro

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # iOS-Resources
+
 This repository compiles a list of valuable resources for learning and mastering iOS development. Each resource is categorized for convenience.
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,60 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import AutoImport from 'astro-auto-import';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'iOS Resources',
+    customCss: [
+        './src/styles/theme.css',
+        './src/styles/custom.css'
+    ],
+    social: {
+      github: 'https://github.com/szymon-michalak/iOS-Resources'
+    },
+    sidebar: [{
+      label: '🖊️ Blogs',
+      autogenerate: {
+        directory: 'blogs'
+      }
+    }, {
+      label: '🎥 Channels',
+      autogenerate: {
+        directory: 'channels'
+      }
+    }, {
+      label: '🎓 Courses',
+      autogenerate: {
+        directory: 'courses'
+      }
+    }, {
+      label: '📚 Books',
+      autogenerate: {
+        directory: 'books'
+      }
+    }, {
+      label: '📂 Repositories',
+      autogenerate: {
+        directory: 'repositories'
+      }
+    }, {
+      label: '🛠️ Tools',
+      autogenerate: {
+        directory: 'tools'
+      }
+    }]
+  }),
+  AutoImport({
+    imports: [
+      {
+        '@astrojs/starlight/components': ['LinkCard', 'CardGrid', 'Card'],
+      },
+    ],
+  }),
+
+  // Make sure the MDX integration is included AFTER astro-auto-import
+  mdx(),
+]
 });
