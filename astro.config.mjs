e871ff3c370e6f91c9c9e530925cb29ca87eac49
Szymon Michalak <77302574+Szymon-Michalak@users.noreply.github.com>
2024-07-11 23:50:37 +0200

    Add github pages subdirectory base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://szymon-michalak.github.io',
+  base: 'iOS-Resources',
   integrations: [starlight({
     title: 'iOS Resources',
     customCss: [
